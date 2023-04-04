f94b79d81b75f021c654f85d5afe1c3e1afdd250
aleksandr105 <aleksandrsherbina105@gmail.com>
2023-04-05 01:41:49 +0200

    adds settings

---
+++ src/components/App/App.jsx
@@ -0,0 +1,7 @@
+import { Wrapper } from "./App.styled";
+
+const App = () => {
+  return <Wrapper className="App">hello world</Wrapper>;
+};
+
+export default App;
---
+++ src/components/App/App.styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  font-size: 40px;
+  width: 250px;
+  text-align: center;
+  margin: 50px auto 0px auto;
+  background-color: red;
+`;
