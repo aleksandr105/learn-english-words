a5ce6b003baa34ae5a0d67edf487d6e82bebaad4
aleksandr105 <aleksandrsherbina105@gmail.com>
2023-04-27 19:34:23 +0200

    test fix

--- src/hooks/useSpeaker.js
+++ src/hooks/useSpeaker.js
@@ -1,27 +1,45 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
+
+// export const useSpeaker = () => {
+//   const [voices, setVoices] = useState([]);
+
+//   const speaker = new SpeechSynthesisUtterance();
+
+//   useEffect(() => {
+//     if (!voices.length) {
+//       setVoices(window.speechSynthesis.getVoices());
+
+//       console.log("fsdfsdf");
+//     }
+//   }, [voices]);
+//   console.log(voices);
+//   const speak = ({ text, volume = 1, rate = 1, pitch = 0, lang = 5 }) => {
+
+//     speaker.voice = voices[lang];
+//     speaker.text = text.trim();
+//     speaker.volume = volume;
+//     speaker.rate = rate;
+//     speaker.pitch = pitch;
+//     window.speechSynthesis.speak(speaker);
+//     return new Promise((resolve) => (speaker.onend = () => resolve()));
+//   };
+
+//   return [speak, voices];
+// };
 
 export const useSpeaker = () => {
-  const [voices, setVoices] = useState([]);
-
   const speaker = new SpeechSynthesisUtterance();
 
-  useEffect(() => {
-    if (!voices.length) {
-      setVoices(window.speechSynthesis.getVoices());
+  let voices = speechSynthesis.getVoices();
 
-      console.log("fsdfsdf");
-    }
-  }, [voices]);
-  console.log(voices);
   const speak = ({ text, volume = 1, rate = 1, pitch = 0, lang = 5 }) => {
-    speaker.voice = voices[lang];
+    speaker.voice = voices[5];
     speaker.text = text.trim();
     speaker.volume = volume;
     speaker.rate = rate;
     speaker.pitch = pitch;
-    window.speechSynthesis.speak(speaker);
+    speechSynthesis.speak(speaker);
     return new Promise((resolve) => (speaker.onend = () => resolve()));
   };
-
   return [speak, voices];
 };
