438e079e95584f2912ab95e6eed7857773c2e9b2
aleksandr105 <aleksandrsherbina105@gmail.com>
2023-05-31 21:18:45 +0200

    adds not show google register link with success register

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { RegisterForm } from "../../components/RegisterForm/RegisterForm";
 import { GoogleAutorizeLink } from "../../components/GoogleAutorizeLink/GoogleAutorizeLink";
+import { successRegister } from "../../redux/auth/selectors";
+import { useSelector } from "react-redux";
 
 const Signup = () => {
+  const isRegister = useSelector(successRegister);
+
   return (
     <>
       <RegisterForm />
-      <GoogleAutorizeLink />
+      {!isRegister && <GoogleAutorizeLink />}
     </>
   );
 };
