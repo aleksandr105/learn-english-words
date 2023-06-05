6335f99c90b2f2425bb2738e7e4396144d42993f
aleksandr105 <aleksandrsherbina105@gmail.com>
2023-06-05 22:04:25 +0200

    fix

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { RegisterForm } from "../../components/RegisterForm/RegisterForm";
 import { GoogleAutorizeLink } from "../../components/GoogleAutorizeLink/GoogleAutorizeLink";
-import { successRegister } from "../../redux/auth/selectors";
-import { useSelector, isLoading } from "react-redux";
+import { successRegister, isLoading } from "../../redux/auth/selectors";
+import { useSelector } from "react-redux";
 
 const Signup = () => {
   const isRegister = useSelector(successRegister);
