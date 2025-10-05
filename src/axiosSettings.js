e2d5488449be049c5a7e12adf517d8232b98a8d4
Aleksandr <aleksandrsherbina105@gmail.com>
2025-10-05 04:35:55 +0200

    edit serwers url

--- src/axiosSettings.js
+++ src/axiosSettings.js
@@ -1,6 +1,6 @@
-import axios from "axios";
+import axios from 'axios';
 
-export const URL = "https://english-words-api.glitch.me/api";
+export const URL = 'http://194.164.207.163:3001';
 
 axios.defaults.baseURL = URL;
 
@@ -8,33 +8,33 @@ export const instance = axios.create({
   baseURL: URL,
 });
 
-instance.interceptors.request.use((config) => {
-  const getAccessToken = localStorage.getItem("accessToken");
+instance.interceptors.request.use(config => {
+  const getAccessToken = localStorage.getItem('accessToken');
 
   config.headers.Authorization = `Bearer ${getAccessToken}`;
   return config;
 });
 
 instance.interceptors.response.use(
-  (config) => config,
-  async (error) => {
+  config => config,
+  async error => {
     const originalRequest = error.config;
     const errorMessage = error.response.data.message;
 
     if (
       error.response.status === 401 &&
       error.config &&
-      (errorMessage === "invalid token or not authorized" ||
-        errorMessage === "not authorized" ||
-        errorMessage === "Refresh token expired")
+      (errorMessage === 'invalid token or not authorized' ||
+        errorMessage === 'not authorized' ||
+        errorMessage === 'Refresh token expired')
     ) {
-      const refreshToken = localStorage.getItem("refreshToken");
+      const refreshToken = localStorage.getItem('refreshToken');
 
-      if (refreshToken === "null" || !refreshToken) return;
+      if (refreshToken === 'null' || !refreshToken) return;
 
       try {
         const { data } = await axios.post(
-          "/auth/refresh",
+          '/auth/refresh',
           {},
           {
             headers: {
@@ -43,9 +43,9 @@ instance.interceptors.response.use(
           }
         );
 
-        localStorage.setItem("refreshToken", data.refreshToken);
+        localStorage.setItem('refreshToken', data.refreshToken);
 
-        localStorage.setItem("accessToken", data.accessToken);
+        localStorage.setItem('accessToken', data.accessToken);
 
         return instance.request(originalRequest);
       } catch (error) {}
